c0f00d84488e772066af48ba3dd23ed0dc8db60c
hwren <hwren2012@163.com>
2025-03-29 21:02:45 +0800

    增加话题配置及测试结果

--- weibo/settings.py
+++ weibo/settings.py
@@ -12,7 +12,7 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'XSRF-TOKEN=ZRmhrf0TlP7q_aBO1A3ahpvn; ALF=1745841073; SUB=_2A25K467hDeRhGeVO71AX9S3OyzyIHXVpgK4prDV8PUJbkNB-LXjAkW1NTTW7dIxwkJ5nFCx5iq0ot2lZK2oIozSt; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhPT0Nommk.Um8bdM1VRAFQ5JpX5KzhUgL.Foe7ShzcSKeEeh52dJLoIEXLxK.L1hzLBKeLxK-LBo5L12qLxK-LB-BL1K5LxKML1-eL1-qLxKBLB.2L12et; _s_tentry=weibo.com; Apache=5998179196579.798.1743249094658; SINAGLOBAL=5998179196579.798.1743249094658; ULV=1743249094673:1:1:1:5998179196579.798.1743249094658:; WBPSESS=iJYS67PKxKWewuCCWzsNZlXjphBkPxWgTdkq9QV7dL0j7DW6o7Kgj-SMYvJvnsYtOx6Cz_h_xtBv2UIpLQDBv-tFh_U2myQ8tX5C-cOW4b6hky5hABZdDvf_dzn64Yy3Qa6SOyJhh86Ofhz5jZtQHA=='
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,7 +24,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['哪吒2']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,12 +33,12 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2025-03-28'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2025-03-28'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
-FURTHER_THRESHOLD = 46
+FURTHER_THRESHOLD = 50
 # 图片文件存储路径
 IMAGES_STORE = './'
 # 视频文件存储路径
